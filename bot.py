e29f8808561a4d4af936e519b4b45d2a4fd50d6d
Dev <dpuranda@ucsc.edu>
2023-10-04 20:54:12 -0700

    Maybe this works?

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from mastodon import Mastodon
+import os
 
 mastodon = Mastodon (
-    access_token = 'token.secret',
+    access_token = os.environ.get("SECRET_KEY"),
     api_base_url = 'https://botsin.space/'
     )
 media = mastodon.media_post("media/thursday.jpg", description="Nadia from the Russian Doll fearing that she would never see a Thursday again says, \"Thursday. What a concept\" while smoking a cigarette")
