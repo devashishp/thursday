7cab93895b1b81b857091f9585db9f4409053c68
Dev <dpuranda@ucsc.edu>
2023-10-04 20:32:20 -0700

    Made it simpler

--- bot.py
+++ bot.py
@@ -1,23 +1,9 @@
-from flask import Flask
 from mastodon import Mastodon
-import schedule
-import time
 
+mastodon = Mastodon (
+    access_token = 'token.secret',
+    api_base_url = 'https://botsin.space/'
+    )
+media = mastodon.media_post("media/thursday.jpg", description="Nadia from the Russian Doll fearing that she would never see a Thursday again says, \"Thursday. What a concept\" while smoking a cigarette")
+mastodon.status_post("Thursday. What a concept.", media_ids=[media])
 
-app = Flask(__name__)
-
-def job():
-    mastodon = Mastodon (
-        access_token = 'token.secret',
-        api_base_url = 'https://botsin.space/'
-        )
-    media = mastodon.media_post("media/thursday.jpg", description="Nadia from the Russian Doll fearing that she would never see a Thursday again says, \"Thursday. What a concept\" while smoking a cigarette")
-    mastodon.status_post("Thursday. What a concept.", media_ids=[media])
-
-schedule.every().thursday.at("00:00").do(job)
-schedule.every().thursday.at("14:00").do(job)
-schedule.every().tuesday.at("23:54").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
