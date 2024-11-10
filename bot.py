89f98fde91337f50b12ef9602b701641f1045aa8
Dev Purandare <devashishpurandare@gmail.com>
2024-11-10 20:54:41 +0530

    Moved to the new account

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 mastodon = Mastodon (
     access_token = os.environ.get("SECRET_KEY"),
-    api_base_url = 'https://botsin.space/'
+    api_base_url = 'https://mastodon.social/'
     )
 media = mastodon.media_post("media/thursday.jpg", description="Nadia from the Russian Doll fearing that she would never see a Thursday again says, \"Thursday. What a concept\" while smoking a cigarette")
 mastodon.status_post("Thursday. What a concept.", media_ids=[media])
